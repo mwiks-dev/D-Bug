ba669a79c87d4256d82830bd5270f0025849190a
Maryann <mwikali119@gmail.com>
2022-06-15 20:35:48 +0300

    Refined serializers code

--- D-Bug-Django/D_Bug_App/serializers.py
+++ D-Bug-Django/D_Bug_App/serializers.py
@@ -2,17 +2,6 @@ from rest_framework import serializers
 from .models import Post, Comments,LANGUAGE_CHOICES
 
 class PostSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=True, allow_blank=False, max_length=100)
-    body = serializers.CharField(required=True, allow_blank=False, max_length=500)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-
-    def create(self, validated_data):
-        return Post.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.body = validated_data.get('body', instance.body)
-        instance.language = validated_data.get('language', instance.language)
-        instance.save()
-        return instance
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'body', 'created','language']
