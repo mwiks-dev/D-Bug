91894bbfed1893aee03d15291907fb77df42144d
Maryann <mwikali119@gmail.com>
2022-06-15 20:36:15 +0300

    Configured urls for posts

--- D-Bug-Django/D_Bug_App/urls.py
+++ D-Bug-Django/D_Bug_App/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('posts/', views.post_list),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
