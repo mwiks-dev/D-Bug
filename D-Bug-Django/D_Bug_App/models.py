220b9e9d60b91b5941719f205c1a014f9236415c
Maryann <mwikali119@gmail.com>
2022-06-15 19:16:20 +0300

    Created post and comments in the models

--- D-Bug-Django/D_Bug_App/models.py
+++ D-Bug-Django/D_Bug_App/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
 
 # Create your models here.
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+
+class Post(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+    class Meta:
+        ordering = ('created',)
+
+class Comments(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', related_name='comments', on_delete=models.CASCADE)
+    class Meta:
+        ordering = ('created',)
