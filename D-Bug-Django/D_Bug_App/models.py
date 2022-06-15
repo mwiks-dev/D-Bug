9153a3b7fc0a2e560093b306a96292972f36136d
Maryann <mwikali119@gmail.com>
2022-06-15 20:34:19 +0300

    Got rid of styles in models

--- D-Bug-Django/D_Bug_App/models.py
+++ D-Bug-Django/D_Bug_App/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from pygments.lexers import get_all_lexers
-from pygments.styles import get_all_styles
 
 # Create your models here.
 LEXERS = [item for item in get_all_lexers() if item[1]]
