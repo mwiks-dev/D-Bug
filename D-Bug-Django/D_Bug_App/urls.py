c5dae6bb293cfa1fa1a86b0dd7c2d6333ff0df8f
Maryann <mwikali119@gmail.com>
2022-06-16 08:48:07 +0300

    Configured url patterns

--- D-Bug-Django/D_Bug_App/urls.py
+++ D-Bug-Django/D_Bug_App/urls.py
@@ -1,8 +1,11 @@
 from . import views
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts/', views.post_list),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
