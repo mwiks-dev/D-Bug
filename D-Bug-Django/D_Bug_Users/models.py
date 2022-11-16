50c4cc4559ef64c6212e6dcfee879035eb31455a
Maryann Mwikali <mwikali119@gmail.com>
2022-11-16 11:21:15 +0300

    Create profile model and serializer

--- D-Bug-Django/D_Bug_Users/models.py
+++ D-Bug-Django/D_Bug_Users/models.py
@@ -1,5 +1,17 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from cloudinary.models import CloudinaryField
 
-class User(AbstractUser):
-    pass
+# class User(AbstractUser):
+#     pass
+
+class Profile(models.Model):
+    name = models.CharField(max_length=15,null=False)
+    bio = models.TextField(max_length=200,null=False)
+    email = models.EmailField()
+    profile_picture = models.CloudinaryField('profile_picture',blank=True)
+    languages = models.TextField(null=False)
+
+    def __str__(self):
+        return self.name
 
