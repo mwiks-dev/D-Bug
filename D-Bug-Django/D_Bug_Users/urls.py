fd8f14bec8f05415a94a75de2f1801a0ad580d46
Maryann Mwikali <mwikali119@gmail.com>
2022-11-18 14:06:05 +0300

    Write a function that finds languages adept to a user from GitHub

--- D-Bug-Django/D_Bug_Users/urls.py
+++ D-Bug-Django/D_Bug_Users/urls.py
@@ -3,5 +3,6 @@ from . import views
 from .views import *
 
 urlpatterns = [
-    path('profile/',views.ProfileFunc,name='profile')
+    path('profile/',views.ProfileFunc,name='profile'),
+    path('profile/<str:name>/',views.ProfileDetails,name="Profile_details")
 ]
--- D-Bug-Django/D_Bug_Users/views.py
+++ D-Bug-Django/D_Bug_Users/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 from .serializers import *
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from .models import *
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse
 from rest_framework import status
+import requests
+
 
 # Create your views here.
 @api_view(['GET','POST'])
@@ -23,3 +24,40 @@ def ProfileFunc(request,format=None):
         profiles = Profile.objects.all()
         profiles = ProfileSerializer(profiles,many=True)
         return Response(profiles.data)
+
+@api_view(['GET','PUT','DELETE'])
+def ProfileDetails(request,name,format=None):
+    try:
+        profile = Profile.objects.get(name=name)
+    except Profile.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        profile = ProfileSerializer(profile,data=data)
+        if profile.is_valid():
+            profile.save()
+            return JsonResponse(profile.data)
+        return JsonResponse(profile.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        profile.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+owner = 'thesmartcoder7'
+access_token = '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'
+headers = {'Authorization':'Token' + access_token}
+
+url = f'https://api.github.com/users/{owner}/repos'
+repos = requests.get(url,headers=headers).json()
+lans = [ls['language'] for ls in repos]
+languages = dict()
+for lan in lans:
+    if lan in languages:
+        languages[lan] += 1
+    else:
+        languages[lan] = 1
+    
+print(languages)
